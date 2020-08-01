e70139155fa69dd0b85153dca099f5e9aa05e010
Maeesha <maeesha.rahman@gmail.com>
2020-07-31 21:05:11 -0400

    adding unique keys to components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
 import Pet from './Pet';
+import pf from 'petfinder-client'
 import './App.css';
 
-class App extends Component {
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
 
-  handleTitleClick = () => {
-    alert("you clicked the title")
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: [],
+    }
   }
+
+  componentDidMount() {
+    petfinder.pet.find({ output: "full", location: "Toronto, ON" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets: pets
+        })
+      })
+  }
+
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <h1>Adopt Me!</h1>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ")
+            } else {
+              breed = pet.breeds.breed
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+              />
+            )
+          })}
+        </div>
       </div>
     )
 
